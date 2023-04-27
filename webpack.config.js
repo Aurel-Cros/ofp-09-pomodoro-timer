c088781ccd9a4e346264e91be901f0ee2a4f4bf7
Aurélien Cros <aure.cros@gmail.com>
2023-04-27 15:58:02 +0200

    fix/main : Webpack config
    Commented out watchFiles, not necessary

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const config = {
         static: {
             directory: path.join(__dirname, 'dist'),
         },
-        watchFiles: ['dist/*']
+        // watchFiles: ['dist/*']
     },
     plugins: [
         new HtmlWebpackPlugin({
