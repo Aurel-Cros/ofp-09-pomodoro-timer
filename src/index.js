86aaea70d297160b081debb5cca87488c926bf9d
Aurélien Cros <aure.cros@gmail.com>
2023-05-04 10:20:11 +0200

    fix/main : fonts
    Reverted to using Google API to fetch fonts instead of importing files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import './body.css';
 import './scss/main.scss';
 import './scss/theme.scss';
 
-import './assets/fonts/BrunoAce-Regular.ttf';
-import './assets/fonts/Oswald-Regular.ttf';
-import './assets/fonts/Prompt-Regular.ttf';
-import './assets/fonts/RumRaisin-Regular.ttf';
-import './assets/fonts/Suravaram-Regular.ttf';
-
 import './assets/audio/alert.mp3';
 
 import { PomodoroTimer } from "./js/pomodoroTimer";
